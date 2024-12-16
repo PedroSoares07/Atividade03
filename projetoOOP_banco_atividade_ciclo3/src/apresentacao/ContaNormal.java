c5d3a8f4da69a7c4c979b08479e8a7dcc7e84221
PedroSoares07 <pedroca71.carvalhos@gmail.com>
2024-12-16 20:06:19 -0300

    Add files via upload

---
+++ projetoOOP_banco_atividade_ciclo3/src/apresentacao/ContaNormal.java
@@ -0,0 +1,21 @@
+package apresentacao;
+
+public class ContaNormal extends Conta {
+
+  public void creditar(double valor) {
+    saldo = saldo + valor;
+  }
+
+  public void debitar(double valor) { // Conta Normal n�o possui limite, logo em uma opera��o de d�bito apenas o saldo
+                                      // est� dispon�vel.
+    if ((saldo - valor) >= 0) {
+      saldo = saldo - valor;
+    }
+
+  }
+
+  @Override
+  public int compareTo(Conta o) {
+    return numero.compareTo(o.getNumero());
+  }
+}
